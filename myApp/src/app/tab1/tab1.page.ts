9371e39053d1e6417eeaee45e162845d3516bf5f
kahramankaradavut <117511123+kahramankaradavut@users.noreply.github.com>
2025-02-24 10:48:37 +0300

    2

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';         
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { camera } from 'ionicons/icons';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,
+    IonFab,
+    IonFabButton,
+    IonIcon
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  photo: string | undefined;
+
+  constructor() {
+    addIcons({ add });
+    addIcons({ camera });
+  }
+
+  async takePhoto() {
+    try {
+      const image = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 90,
+      });
+
+      this.photo = image.webPath; // Fotoğrafın URI'sini kaydet
+      console.log('Photo taken:', image);
+    } catch (error) {
+      console.error('Error taking photo:', error);
+    }
+  }
 }
